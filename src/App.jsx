5a14698fc4d9a5386a268ce3bc43c896de349527
vule04 <vuule.04@gmail.com>
2026-03-30 23:20:09 -0500

    added routing for cashier view

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Menu from './Menu'
 import './App.css'
+import CashierView from './CashierView'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Menu" element={<Menu />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cashier-view" element={<CashierView />}/>
       </Routes>
     </Router>
   )
