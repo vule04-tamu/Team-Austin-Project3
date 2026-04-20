521c8daeda468ebc03cd952554c2e4bc0e5a7d34
vule04 <vuule.04@gmail.com>
2026-04-19 21:18:00 -0500

    added dynamic font size

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Menu from './Menu'
+import TextSizeControl from './TextSizeControl'
 import './App.css'
 import CashierView from './CashierView'
 import ManagerView from './ManagerView'
@@ -9,13 +10,18 @@ import CustomerView from './CustomerView'
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/cashier" element={<CashierView />}/>
-        <Route path="/manager" element={<ManagerView />}/>
-        <Route path="/customer" element={<CustomerView />}/>
-      </Routes>
+      <div className="app-container">
+        <div className="app-header">
+          <TextSizeControl />
+        </div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cashier" element={<CashierView />}/>
+          <Route path="/manager" element={<ManagerView />}/>
+          <Route path="/customer" element={<CustomerView />}/>
+        </Routes>
+      </div>
     </Router>
   )
 }
